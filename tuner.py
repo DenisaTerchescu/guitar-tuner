fe1b193fe7d35face86b84f9f464abf97428ffcd
Constantin-Iulian <radoi644@gmail.com>
2023-04-02 20:50:39 +0300

    updated tags

--- tuner.py
+++ tuner.py
@@ -5,42 +5,34 @@ import scipy.fftpack
 import sounddevice as sd
 import time
 
-# General settings that can be changed by the user
-SAMPLE_FREQ = 48000 # sample frequency in Hz
-WINDOW_SIZE = 48000 # window size of the DFT in samples
-WINDOW_STEP = 12000 # step size of window
-NUM_HPS = 5 # max number of harmonic product spectrums
-POWER_THRESH = 1e-6 # tuning is activated if the signal power exceeds this threshold
+# Setari generale ale tunerului
+SAMPLE_FREQ = 48000 # frecventa proba in Hz
+WINDOW_SIZE = 48000 # marime fereastra DFT in esantioane
+WINDOW_STEP = 12000 # marime step 
+NUM_HPS = 5         # numar maxim de HPS (Harmonic Product Spectrum)
+POWER_THRESH = 1e-6 # tunerul e activat daca puterea semnalului este mai mare decat acest prag
 CONCERT_PITCH = 440 # defining a1
-WHITE_NOISE_THRESH = 0.2 # everything under WHITE_NOISE_THRESH*avg_energy_per_freq is cut off
+WHITE_NOISE_THRESH = 0.2 # orice sub WHITE_NOISE_THRESH*avg_energy_per_freq e scos 
 
-WINDOW_T_LEN = WINDOW_SIZE / SAMPLE_FREQ # length of the window in seconds
-SAMPLE_T_LENGTH = 1 / SAMPLE_FREQ # length between two samples in seconds
-DELTA_FREQ = SAMPLE_FREQ / WINDOW_SIZE # frequency step width of the interpolated DFT
+WINDOW_T_LEN = WINDOW_SIZE / SAMPLE_FREQ # lungime fereastra in secunde
+SAMPLE_T_LENGTH = 1 / SAMPLE_FREQ        # durata unui esantion in secunde
+DELTA_FREQ = SAMPLE_FREQ / WINDOW_SIZE   # rezolutia spectrului in Hz
 OCTAVE_BANDS = [50, 100, 200, 400, 800, 1600, 3200, 6400, 12800, 25600]
 
 ALL_NOTES = ["A","A#","B","C","C#","D","D#","E","F","F#","G","G#"]
+HANN_WINDOW = np.hanning(WINDOW_SIZE)
+
+
 def find_closest_note(pitch):
-  """
-  This function finds the closest note for a given pitch
-  Parameters:
-    pitch (float): pitch given in hertz
-  Returns:
-    closest_note (str): e.g. a, g#, ..
-    closest_pitch (float): pitch of the closest note in hertz
-  """
   i = int(np.round(np.log2(pitch/CONCERT_PITCH)*12))
+
   closest_note = ALL_NOTES[i%12] + str(4 + (i + 9) // 12)
   closest_pitch = CONCERT_PITCH*2**(i/12)
+
   return closest_note, closest_pitch
 
-HANN_WINDOW = np.hanning(WINDOW_SIZE)
+
 def callback(indata, frames, time, status):
-  """
-  Callback function of the InputStream method.
-  That's where the magic happens ;)
-  """
-  # define static variables
   if not hasattr(callback, "window_samples"):
     callback.window_samples = [0 for _ in range(WINDOW_SIZE)]
   if not hasattr(callback, "noteBuffer"):
@@ -49,27 +41,28 @@ def callback(indata, frames, time, status):
   if status:
     print(status)
     return
-  if any(indata):
-    callback.window_samples = np.concatenate((callback.window_samples, indata[:, 0])) # append new samples
-    callback.window_samples = callback.window_samples[len(indata[:, 0]):] # remove old samples
 
-    # skip if signal power is too low
+  if any(indata):
+    callback.window_samples = np.concatenate((callback.window_samples, indata[:, 0])) # adauga esantioane noi
+    callback.window_samples = callback.window_samples[len(indata[:, 0]):] # sterge esantioanele vechi
+
+    
     signal_power = (np.linalg.norm(callback.window_samples, ord=2, axis=0)**2) / len(callback.window_samples)
     if signal_power < POWER_THRESH:
       os.system('cls' if os.name=='nt' else 'clear')
       print("Closest note: ...")
       return
 
-    # avoid spectral leakage by multiplying the signal with a hann window
+    # evita spectral leakage prin inmultirea semnalului cu o fereastra hann
     hann_samples = callback.window_samples * HANN_WINDOW
     magnitude_spec = abs(scipy.fftpack.fft(hann_samples)[:len(hann_samples)//2])
 
-    # supress mains hum, set everything below 62Hz to zero
+    # sterge orice sub 62 Hz
     for i in range(int(62/DELTA_FREQ)):
       magnitude_spec[i] = 0
 
-    # calculate average energy per frequency for the octave bands
-    # and suppress everything below it
+    # calculeaza energia medie pe fiecare banda de octave
+    # si sterge orice sub WHITE_NOISE_THRESH*avg_energy_per_freq
     for j in range(len(OCTAVE_BANDS)-1):
       ind_start = int(OCTAVE_BANDS[j]/DELTA_FREQ)
       ind_end = int(OCTAVE_BANDS[j+1]/DELTA_FREQ)
@@ -79,14 +72,14 @@ def callback(indata, frames, time, status):
       for i in range(ind_start, ind_end):
         magnitude_spec[i] = magnitude_spec[i] if magnitude_spec[i] > WHITE_NOISE_THRESH*avg_energy_per_freq else 0
 
-    # interpolate spectrum
+    # interpoleaza spectrul pentru a avea un numar de esantioane fix
     mag_spec_ipol = np.interp(np.arange(0, len(magnitude_spec), 1/NUM_HPS), np.arange(0, len(magnitude_spec)),
                               magnitude_spec)
-    mag_spec_ipol = mag_spec_ipol / np.linalg.norm(mag_spec_ipol, ord=2, axis=0) #normalize it
+    mag_spec_ipol = mag_spec_ipol / np.linalg.norm(mag_spec_ipol, ord=2, axis=0) # normalizare
 
     hps_spec = copy.deepcopy(mag_spec_ipol)
 
-    # calculate the HPS
+    # calculeaza HPS (Harmonic Product Spectrum)
     for i in range(NUM_HPS):
       tmp_hps_spec = np.multiply(hps_spec[:int(np.ceil(len(mag_spec_ipol)/(i+1)))], mag_spec_ipol[::(i+1)])
       if not any(tmp_hps_spec):
@@ -110,11 +103,12 @@ def callback(indata, frames, time, status):
       print(f"Closest note: ...")
 
   else:
-    print('no input')
+    print('no input detected')
 
 try:
   print("Starting HPS guitar tuner...")
-  with sd.InputStream(channels=1, callback=callback, blocksize=WINDOW_STEP, samplerate=SAMPLE_FREQ):
+  print(sd.query_devices())
+  with sd.InputStream(channels=1, callback=callback, blocksize=WINDOW_STEP, samplerate=SAMPLE_FREQ, device=4):
     while True:
       time.sleep(0.5)
 except Exception as exc:
